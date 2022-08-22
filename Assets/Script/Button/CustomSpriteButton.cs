fb99260653a6e7ee7fdb652faf0bfe4743fa1b7b
tunakaniri <tunacanirimb@gmail.com>
2022-08-22 22:48:50 +0900

    Add CustomSpriteButton Script

---
+++ Assets/Script/Button/CustomSpriteButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CustomSpriteButton : MonoBehaviour
+{
+    [SerializeField] Sprite Highlighted;
+    [SerializeField] Sprite Pressed;
+    [SerializeField] Sprite Selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
